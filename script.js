4bf2fdaf1fc21981eebda83ee7d33a49d6ebc2b8
JAPI330 <166940642+JAPI330@users.noreply.github.com>
2024-04-13 18:37:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const timeString = hours + ':' + minutes;
+    document.getElementById('clock').textContent = timeString;
+}
+
+setInterval(updateTime, 1000);
